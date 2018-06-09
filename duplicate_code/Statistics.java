968d2776162f73e9f733035dab077dada8682127
a23879529 <a23879529@gmail.com>
2018-06-09 16:00:18 +0800

    dup

--- duplicate_code/Statistics.java
+++ duplicate_code/Statistics.java
@@ -4,23 +4,17 @@ public class Statistics
 {
     public double calculateDifferenceOfAverage(double[] array1, double[] array2)
     {
-        double sum1 = 0;
-        double average1;
+	    return Math.abs(calAvg(array1) - calAvg(array2));
+    }
 
-        for (double element : array1) {
-            sum1 += element;
-        }
-        average1 = sum1 / array1.length;
-
-        double sum2 = 0;
-        double average2;
-
-        for (double element : array2) {
-            sum2 += element;
-        }
-        average2 = sum2 / array2.length;
-
-        return Math.abs(average1 - average2);
+    public double calAvg(double[] target){
+    	double sum = 0;
+	double average;
+	for (double element : target){
+		sum += element;
+	}
+	average = target / target.length;
+	return average;
     }
 
     public double calculateSampleVariance(double[] elements)
