e28daabc833b7a2b7f082644f3ad5dd7dec2112c
1080321 <q0929801868w@gmail.com>
2019-05-25 15:53:45 +0800

    large_class

--- large_class/Employee.java
+++ large_class/Employee.java
@@ -1,4 +1,5 @@
 package com.directi.training.codesmells.largeclass;
+package com.directi.training.codesmells_refactored.largeclass;
 
 public class Employee
 {
@@ -6,19 +7,24 @@ public class Employee
     private final String _officeAreaCode;
     private final String _officeNumber;
     private final String _officeExtensionNumber;
+    private final TelephoneNumber _telephoneNumber;
 
     public Employee(String name, String officeAreaCode, String officeNumber,
                     String officeExtensionNumber)
+    public Emploee(String name, TelephoneNumber telephoneNumber)
     {
         _name = name;
         _officeAreaCode = officeAreaCode;
         _officeNumber = officeNumber;
         _officeExtensionNumber = officeExtensionNumber;
+        _telephoneNumber = telephoneNumber
     }
 
     public String getTelephoneNumber()
+    public TelephoneNumber getTelephoneNumber()
     {
         return "(" + _officeAreaCode + ") " + _officeNumber + "-" + _officeExtensionNumber;
+        return _telephoneNumber
     }
 
     public String getName()
