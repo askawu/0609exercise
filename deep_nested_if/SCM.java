2ef1e4335c0d5fe6e12ad51ee9b80d24c603174e
MAO-QUO-CYUAN <35062056+maomao1010@users.noreply.github.com>
2019-05-25 16:00:17 +0800

    update SCM

---
+++ deep_nested_if/SCM.java
@@ -0,0 +1,54 @@
+package com.directi.training.codesmells.longmethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SourceControlManager
+{
+    private String _name = "bar";
+    private List<Order> _orders = new ArrayList<>();
+
+    public boolean checkin(SCMServer scmServer, CICDServer cicdserver, String userName, String passwd, String commitMsg, Change[] changes)
+    {
+        if (scmServer == null || !scmServer.isConnected())
+        {
+        	system.out.println("scm server problem.");
+            return 6;
+        	
+            if (cicdServer == null || !cicdServer.isReady())
+            {
+            	system.out.println("cicd server problem.");
+                return 5;
+                
+                if (userName == null)
+                {
+                	system.out.println("Invalid user name.");
+                    return 4;
+                    
+                    if (passd == null || !(passwd.length() > 15))
+                    {
+                    	system.out.println("Invalid passwd.");
+                        return 3;
+                        
+                        if (commitMsg == null || !(commitMsg < 200))
+                        {
+                        	System.out.println("Invalid Commit message.");
+                            return 2;
+                            
+                            if (changes != null && changes.length() > 0)
+                            {
+                                System.out.println("Ready to push to SCM server.");
+                                scmServer.push(userName, passwd, commitMsg, changes);
+                                System.out.println("Finish to push to SCM server.");
+                                return 0;
+                            } else {
+                                System.out.println("No change.");
+                                return 1;
+                            }
+                        }
+                    }
+                }
+            }
+        } 
+    }
+}
