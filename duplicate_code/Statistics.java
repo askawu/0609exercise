0248148786c83ecb7ffb28f8106bbaf8a5176c67
1080321 <q0929801868w@gmail.com>
2019-05-25 16:16:27 +0800

    Average Fuction

--- duplicate_code/Statistics.java
+++ duplicate_code/Statistics.java
@@ -20,17 +20,30 @@ public class Statistics
         }
         average2 = sum2 / array2.length;
 
+        double average1 = getAverage(array1);
+        double average2 = getAverage(array2);
         return Math.abs(average1 - average2);
     }
 
     public double calculateSampleVariance(double[] elements)
+    private double getAverage(double[] array)
     {
         double sum1 = 0;
         double average1;
         for (double element : elements) {
+        for (double element : array)
             sum1 += element;
         }
         average1 = sum1 / elements.length;
+        average1 = sum1 / array.length;
+        return average1;
+
+
+        }
+        public double calculateSampleVariance(double[] elements)
+        {
+
+        double average1 = getAverage(elements);
         double temp = 0.0;
         for (double element : elements) {
             temp += Math.pow(element - average1, 2);
