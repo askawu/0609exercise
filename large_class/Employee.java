8f6933f147fb09f96eb1d66993f1a2c0e9c319cd
1052983 <fanchsin@gmail.com>
2019-05-25 15:58:56 +0800

    fix large class

--- large_class/Employee.java
+++ large_class/Employee.java
@@ -3,26 +3,43 @@ package com.directi.training.codesmells.largeclass;
 public class Employee
 {
     private final String _name;
-    private final String _officeAreaCode;
-    private final String _officeNumber;
-    private final String _officeExtensionNumber;
+    private final TelephoneNumber _telephoneNumber;
 
-    public Employee(String name, String officeAreaCode, String officeNumber,
-                    String officeExtensionNumber)
+    public Employee(String name, TelephoneNumber telephoneNumber)
     {
         _name = name;
-        _officeAreaCode = officeAreaCode;
-        _officeNumber = officeNumber;
-        _officeExtensionNumber = officeExtensionNumber;
+        _telephoneNumber = telephoneNumber;
     }
 
-    public String getTelephoneNumber()
+    public TelephoneNumber getTelephoneNumber()
     {
-        return "(" + _officeAreaCode + ") " + _officeNumber + "-" + _officeExtensionNumber;
+	return _telephoneNumber;
     }
+
 
     public String getName()
     {
         return _name;
     }
+}
+
+public class TelephoneNumber
+{
+    private final String _officeAreaCode;
+    private final String _officeNumber;
+    private final String _officeExtensionNumber;
+
+    public TelephoneNumber(String officeAreaCode, String officeNumber, String officeExtensionNumber)
+    {
+        _officeAreaCode = officeAreaCode;
+        _officeNumber = officeNumber;
+        _officeExtensionNumber = officeExtensionNumber;
+    }
+
+    @override
+    public String toString()
+    {
+        return "(" + officeAreaCode + ") " + officeNumber + "-" + officeExtensionNumber;
+    }
+
 }
