c03cd7e876c6301283e688578674d95bf1d3c826
1070333 <angel482400@gmail.com>
2019-05-25 18:27:56 +0800

    1070333

--- duplicate_code/Statistics.java
+++ duplicate_code/Statistics.java
@@ -2,38 +2,35 @@ package com.directi.training.codesmells.duplicatecode;
 
 public class Statistics
 {
+
+    private double calculateAverage(double[] array)
+    {
+        double sum = 0.0;
+        double average = 0.0;
+
+        for (double element : array) {
+            sum += element;
+        }
+        average = sum / array.length;
+
+        return average;
+    }
+
     public double calculateDifferenceOfAverage(double[] array1, double[] array2)
     {
-        double sum1 = 0;
-        double average1;
-
-        for (double element : array1) {
-            sum1 += element;
-        }
-        average1 = sum1 / array1.length;
-
-        double sum2 = 0;
-        double average2;
-
-        for (double element : array2) {
-            sum2 += element;
-        }
-        average2 = sum2 / array2.length;
+        average1 = calculateAverage(array1);
+        average2 = calculateAverage(array2);
 
         return Math.abs(average1 - average2);
     }
 
     public double calculateSampleVariance(double[] elements)
     {
-        double sum1 = 0;
-        double average1;
-        for (double element : elements) {
-            sum1 += element;
-        }
-        average1 = sum1 / elements.length;
+        average =  calculateAverage(elements);
+
         double temp = 0.0;
         for (double element : elements) {
-            temp += Math.pow(element - average1, 2);
+            temp += Math.pow(element - average, 2);
         }
         return temp / (elements.length - 1);
 
