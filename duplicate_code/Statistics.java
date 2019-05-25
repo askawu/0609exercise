ba4c88c35bc6c7d98141f310622e27995cdaaf2e
朱庭樂 <s1070334@mail.ncyu.edu.tw>
2019-05-25 14:30:25 +0800

    test

--- duplicate_code/Statistics.java
+++ duplicate_code/Statistics.java
@@ -4,36 +4,33 @@ public class Statistics
 {
     public double calculateDifferenceOfAverage(double[] array1, double[] array2)
     {
-        double sum1 = 0;
+        double sum = 0;
         double average1;
-
-        for (double element : array1) {
-            sum1 += element;
-        }
-        average1 = sum1 / array1.length;
-
-        double sum2 = 0;
         double average2;
 
-        for (double element : array2) {
-            sum2 += element;
+        for (double element : array1) {
+            sum += element;
         }
-        average2 = sum2 / array2.length;
+        average1 = sum / array1.length;
+        for (double element : array2) {
+            sum += element;
+        }
+        average2 = sum / array2.length;
 
         return Math.abs(average1 - average2);
     }
 
     public double calculateSampleVariance(double[] elements)
     {
-        double sum1 = 0;
-        double average1;
+        double sum = 0;
+        double average;
         for (double element : elements) {
-            sum1 += element;
+            sum += element;
         }
-        average1 = sum1 / elements.length;
+        average = sum / elements.length;
         double temp = 0.0;
         for (double element : elements) {
-            temp += Math.pow(element - average1, 2);
+            temp += Math.pow(element - average, 2);
         }
         return temp / (elements.length - 1);
 
