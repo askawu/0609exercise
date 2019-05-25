8f33ad027d66c2099c375d9ccc8097e33b86d1c7
MAO-QUO-CYUAN <35062056+maomao1010@users.noreply.github.com>
2019-05-25 14:39:21 +0800

    update static

--- duplicate_code/Statistics.java
+++ duplicate_code/Statistics.java
@@ -4,38 +4,29 @@ public class Statistics
 {
     public double calculateDifferenceOfAverage(double[] array1, double[] array2)
     {
-        double sum1 = 0;
-        double average1;
-
-        for (double element : array1) {
-            sum1 += element;
-        }
-        average1 = sum1 / array1.length;
-
-        double sum2 = 0;
-        double average2;
-
-        for (double element : array2) {
-            sum2 += element;
-        }
-        average2 = sum2 / array2.length;
-
+        double average1 = CaculateAverage(array1);
+        double average2 = CaculateAverage(array2);
         return Math.abs(average1 - average2);
     }
 
     public double calculateSampleVariance(double[] elements)
     {
-        double sum1 = 0;
-        double average1;
-        for (double element : elements) {
-            sum1 += element;
-        }
-        average1 = sum1 / elements.length;
+        double average1 = CaculateAverage(elements);
         double temp = 0.0;
         for (double element : elements) {
             temp += Math.pow(element - average1, 2);
         }
         return temp / (elements.length - 1);
 
+    }
+    private double CaculateAverage (double[] elements){
+        double sum = 0.0;
+        double average = 0.0;
+        
+        for (double element : elements) {
+            sum += element;
+        }
+        average = sum / elements.length;
+        return average;        
     }
 }
