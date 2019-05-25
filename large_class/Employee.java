f0a388bf4fce7f754c22d18039dec7cfa4c5adb9
朱庭樂 <s1070334@mail.ncyu.edu.tw>
2019-05-25 15:13:12 +0800

    test

--- large_class/Employee.java
+++ large_class/Employee.java
@@ -1,11 +1,12 @@
 package com.directi.training.codesmells.largeclass;
 
-public class Employee
+public class Employee extends Telephone
 {
     private final String _name;
     private final String _officeAreaCode;
     private final String _officeNumber;
     private final String _officeExtensionNumber;
+	private final String _Telephone
 
     public Employee(String name, String officeAreaCode, String officeNumber,
                     String officeExtensionNumber)
@@ -14,15 +15,22 @@ public class Employee
         _officeAreaCode = officeAreaCode;
         _officeNumber = officeNumber;
         _officeExtensionNumber = officeExtensionNumber;
+		_Telephone = super(officeAreaCode, officeNumber, officeExtensionNumber);
     }
-
-    public String getTelephoneNumber()
-    {
-        return "(" + _officeAreaCode + ") " + _officeNumber + "-" + _officeExtensionNumber;
-    }
-
+    
     public String getName()
     {
         return _name;
     }
+}
+
+
+public class Telephone
+{
+	public Telephone(){}
+	
+	public Telephone(String officeAreaCode, String officeNumber, String officeExtensionNumber)
+	{
+		return "(" + _officeAreaCode + ") " + _officeNumber + "-" + _officeExtensionNumber;
+	}
 }
